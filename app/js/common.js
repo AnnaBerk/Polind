e1dd943af247334056fc22ca9222e96d83fc710d
Бернова Анна <bast33@yandex.ru>
2019-05-21 22:30:41 +0300

    Added tabs for block best-offers

--- app/js/common.js
+++ app/js/common.js
@@ -30,3 +30,31 @@ entry.addEventListener("mouseout", function(evt){
 })
 });
 
+offer.forEach(function (entry) {
+entry.addEventListener("focus", function(evt){
+	evt.preventDefault();
+	var header = entry.querySelector(".best-offers__header");
+	header.style.marginTop = "170px";
+
+  var place = entry.querySelector(".best-offers__place");
+  place.style.marginBottom = "120px";
+
+	var button = entry.querySelector(".button-best-offer");
+	button.classList.remove("visually-hidden");
+})
+});
+
+offer.forEach(function (entry) {
+entry.addEventListener("blur", function(evt){
+	evt.preventDefault();
+
+	var header = entry.querySelector(".best-offers__header");
+	header.style.marginTop = "320px";
+
+	var place = entry.querySelector(".best-offers__place");
+  place.style.marginBottom = "10px";
+
+	var button = entry.querySelector(".button-best-offer");
+	button.classList.add("visually-hidden");
+})
+});
